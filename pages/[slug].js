4de9f22332cded3627cc8c229845a269fcbfaa7b
Suzan Rana <dev.suzanrana@gmail.com>
2022-10-11 19:03:30 +0545

    initial git commit

---
+++ pages/[slug].js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BlogDetail = () => {
+  return (
+    <div>BlogDetail</div>
+  )
+}
+
+export default BlogDetail;
+
+// "@mdx-js/loader": "^1.6.22",
+// "@next/mdx": "^11.0.1",
+// "gray-matter": "^4.0.3",
+// "marked": "^2.1.3",
+// "next": "11.0.1",
+// "next-mdx-remote": "^3.0.4",
+// "react": "17.0.2",
+// "react-dom": "17.0.2",
+// "react-syntax-highlighter": "^15.4.4"
+// npm i @mdx-js/loader @next/mdx gray-matter marked react-syntax-highlighter
